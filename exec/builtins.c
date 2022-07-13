2988828eada097aec9b6d3f668b81d780e39dc9c
anastabiti <anastabiti@gmail.com>
2022-07-13 18:19:50 +0100

    support echo with -nnnnnnnnnnnnnnnnnnnnnn

--- exec/builtins.c
+++ exec/builtins.c
@@ -3,30 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
+/*   By: atabiti <atabiti@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 09:06:50 by atabiti           #+#    #+#             */
-/*   Updated: 2022/07/13 12:05:09 by atabiti          ###   ########.fr       */
+/*   Updated: 2022/07/13 18:18:46 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include "../minishell.h"
 #include "../parsing/parse.h"
+int	ft_echo_n(t_cmd *cmds, int fd)
+{
+	int	i;
+
+	i = 1;
+	if (cmds->cmd[1] != NULL && cmds->cmd[1][0] == '-'
+		&& cmds->cmd[1][1] == 'n')
+	{
+		while (cmds->cmd[1][i] != '\0')
+		{
+			if (cmds->cmd[1][i] != 'n')
+				return (UNSUCCESSFUL);
+			i++;
+		}
+	}
+	else
+		return (UNSUCCESSFUL);
+	return (SUCCESSFUL);
+}
 
 int	ft_echo_c(t_cmd *cmds, int fd)
 {
 	int	i;
 	int	len;
-
+	int check = UNSUCCESSFUL;
 	i = 1;
 	len = 0;
 	while (cmds->cmd[i])
 	{
+		if (ft_echo_n(cmds, fd) == SUCCESSFUL && i == 1)
+		{
+			check = SUCCESSFUL;
+						i++;
+
+		}
 		len = ft_strlen(cmds->cmd[i]);
-		write(fd, " ", 1);
 		write(fd, cmds->cmd[i], len);
 		i++;
+		if (cmds->cmd[i])
+			write(fd, " ", 1);
+		else if (cmds->cmd[i] == NULL && check == UNSUCCESSFUL)
+				write(fd, "\n", 1);
+
 	}
 	write(fd, "\n", 1);
 	return (SUCCESSFUL);
@@ -44,7 +73,7 @@ int	ft_echo(t_cmd *cmds, int fd)
 		write(fd, "\n", 1);
 		return (SUCCESSFUL);
 	}
-	else
+	else if (cmds->cmd[1] != NULL)
 	{
 		ft_echo_c(cmds, fd);
 	}
