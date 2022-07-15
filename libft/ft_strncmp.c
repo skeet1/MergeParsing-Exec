0a5265c007143bcf1c081c6563697db019fb2075
Mohamed Karim <76565545+skeet1@users.noreply.github.com>
2022-07-15 18:47:08 +0100

    protect strncmp for avoid the segfault

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 12:23:09 by mkarim            #+#    #+#             */
-/*   Updated: 2022/06/27 20:03:42 by mkarim           ###   ########.fr       */
+/*   Updated: 2022/07/15 16:09:00 by mkarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 	i = 0;
 	if (n == 0)
-		return (0);
-	while (s1[i] && s1[i] == s2[i] && i < n - 1)
-		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		return (1);
+	if (s1 && s2)
+	{
+		while (s1[i] && s1[i] == s2[i] && i < n - 1)
+			i++;
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (1);
 }
