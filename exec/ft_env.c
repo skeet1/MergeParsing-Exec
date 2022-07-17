a80eda324c98ed3e0cad6b721c239e96432988e5
Anas Tabiti <atabiti@e1r13p4.1337.ma>
2022-07-17 09:16:36 +0100

    fix

--- exec/ft_env.c
+++ exec/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 10:50:22 by atabiti           #+#    #+#             */
-/*   Updated: 2022/07/17 09:02:19 by atabiti          ###   ########.fr       */
+/*   Updated: 2022/07/17 09:16:06 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,12 @@ int	ft_env(t_lis *envp, t_cmd *cmd)
 	while (envp != NULL)
 	{
 		envval = envp->content;
-		if (envval->value != NULL)
+		if (ft_strlen(envval->value) != 0)
 		{
 			ft_putstr_fd(envval->name, cmd->fd_out);
-		ft_putstr_fd("=", cmd->fd_out);
-		ft_putendl_fd(envval->value, cmd->fd_out);
+			ft_putstr_fd("=", cmd->fd_out);
+			ft_putendl_fd(envval->value, cmd->fd_out);
 		}
-			
 		envp = envp->next;
 	}
 	return (0);
